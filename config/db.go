003cd2bccbfb4e5f43296963e51e562c48794d04
DarKnight <05.DarKnight.11@gmail.com>
2016-05-08 15:12:41 +0530

    replaced postgresql with sqlite3

--- config/db.go
+++ config/db.go
@@ -1,42 +1,30 @@
 package config
 
 import (
+	"database/sql"
 	"log"
 	"os"
+	"path"
 
-	"github.com/BurntSushi/toml"
+	"github.com/mattn/go-sqlite3"
 
 	"health_monitor/utils"
 )
 
-var (
-	dbParameters struct {
-		User     string
-		Password string
-		Database string
-		Address  string
-		Port     string
-	}
-
-	DBInstance *pg.DB //database instance for use in other modules
-)
-
-func loadDBParams() {
-	_, err := toml.DecodeFile(DB.DBConfigFile, &dbParameters)
-	utils.PDBFileError(err) // TODO if this function is not used elsewhere remove it from utils
-}
-
-func init() {
+func db_init() {
+	var DB_DRIVER string
 	file, err := os.OpenFile(Logs.HealthMonitorLog, os.O_RDWR|os.O_CREATE|
 		os.O_APPEND, 0666)
 	utils.PLogError(err)
 	defer file.Close()
 	log.SetOutput(file)
-	loadDBParams()
-	DBInstance = pg.Connect(&pg.Options{
-		User:     dbParameters.User,
-		Password: dbParameters.Password,
-		Database: dbParameters.Database,
-		Addr:     dbParameters.Address + ":" + dbParameters.Port,
-	})
+	sql.Register(DB_DRIVER, &sqlite3.SQLiteDriver{})
+	database, err := sql.Open(DB_DRIVER, path.Join(ConfigDir, "monitor.db"))
+	if err != nil {
+		log.Println("Failed to create the handle")
+	}
+	if err2 := database.Ping(); err2 != nil {
+		log.Println("Failed to keep connection alive")
+	}
+
 }
