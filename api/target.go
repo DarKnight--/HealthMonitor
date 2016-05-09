cc98795fac91540ebf5c3c41b0b51c161442f363
DarKnight <05.DarKnight.11@gmail.com>
2016-05-09 15:02:29 +0530

    [api] Added function to check for the targets which are currently being scanned

---
+++ api/target.go
@@ -0,0 +1,91 @@
+package api
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"health_monitor/config"
+)
+
+type (
+	// Target contains list of targets in the database recieved from OWTF api
+	Target struct {
+		ID        int    `json:"id"`
+		TargetURL string `json:"target_url"`
+	}
+)
+
+// GetTarget function calls to the OWTF api to recieve all the targets in the
+// OWTF database
+func GetTarget() ([]Target, error) {
+	var targets []Target
+	var objmap map[string]*json.RawMessage
+	var err error
+	var path = "/api/targets/search/"
+	// get all the tagrget json data from OWTF target endnode
+	var response *http.Response
+	response, err = http.Get(config.ConfigVars.OWTFAddress + path)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	// Converting data recieved from http request to byte format
+	var dataByte []byte
+	dataByte, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	err = json.Unmarshal(dataByte, &objmap)
+
+	// Converting json byte to targets data structure
+	err = json.Unmarshal(*objmap["data"], &targets)
+	if err != nil {
+		log.Println(err)
+		return targets, err
+	}
+	return targets, nil
+}
+
+// CheckTarget checks whether the target in the database actually under the
+// scan.
+func CheckTarget(target string) bool {
+	var path = "/api/worklist/search?target_url="
+	var err error
+	//var objmap map[string]*json.RawMessage
+	var response *http.Response
+	var data struct {
+		recordsFiltered int `json: records_filtered`
+	}
+
+	response, err = http.Get(config.ConfigVars.OWTFAddress + path + target)
+	if err != nil {
+		log.Println(err)
+		// TODO check for error and if OWTF is down shutdown monitor gracefully
+		return false
+	}
+	defer response.Body.Close()
+	var dataByte []byte
+	dataByte, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	err = json.Unmarshal(dataByte, &data)
+	if err != nil {
+		log.Println("Error occured during decoding")
+		return false
+	}
+
+	if data.recordsFiltered > 0 {
+		return true
+	}
+
+	return false
+}
