1678cbd8373b29b517b7584ff43827365772c690
DarKnight <05.DarKnight.11@gmail.com>
2016-05-14 22:33:47 +0530

    [config] Added function to setup config file

---
+++ config/setup.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"os"
+	"path"
+)
+
+var config = `# Config file for OWTF-HealthMonitor
+
+# Config file should contain absolute paths or file relative to $HOME directory
+
+# HomeDir is the directory where all the logs and config file will reside
+HomeDir = ".owtfMonitor/"
+
+# DBFile is the sqlite database file absolute path
+DBFile = ".owtfMonitor/config/monitor.db"
+
+# OWTFAddress is the address of OWTF API
+OWTFAddress = "http://127.0.0.1:8009"
+
+`
+
+func setupConfig() {
+	var baseDir = path.Join(os.Getenv("HOME"), ".owtfMonitor")
+	var configDir = path.Join(baseDir, "config")
+	var configFile = path.Join(configDir, "config.toml")
+
+	_, err := os.Stat(configDir)
+	if err != nil {
+		// Create the config directory as it does not exists.
+		os.MkdirAll(configDir, 0777)
+	}
+
+	file, _ := os.OpenFile(configFile, os.O_WRONLY|os.O_CREATE, 0666)
+
+	file.WriteString(config)
+}
