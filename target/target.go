52286510eace96e3cf9677dbab4e1c2c64dd7072
DarKnight <05.DarKnight.11@gmail.com>
2016-06-21 06:27:25 +0530

    [target] Added core function

---
+++ target/target.go
@@ -0,0 +1,33 @@
+package target
+
+import (
+	"errors"
+
+	"github.com/valyala/fasthttp"
+)
+
+type (
+	Config struct {
+		Profile          string
+		RecheckThreshold int
+	}
+)
+
+func (conf Config) CheckStatus(target string, hash string) (int, error) {
+	statusCode, body, err := fasthttp.Get(nil, target)
+	if err != nil {
+		return 0, err
+	}
+	if statusCode/100 == 2 {
+		return 0, errors.New("Status code returned was " + string(statusCode))
+	}
+	newHash, err := HashString(body)
+	if err != nil {
+		return 0, err
+	}
+	score := CompareHash(hash, newHash)
+	if score == -1 {
+		return score, errors.New("Error occured while comparing hashes")
+	}
+	return score, nil
+}
