8012e67b8e546a6f7b8b7d7e3a1dce01335aa9ff
DarKnight <05.DarKnight.11@gmail.com>
2016-06-30 07:21:07 +0530

    Shifted package install from apt-get

--- install.go
+++ install.go
@@ -4,20 +4,40 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"health_monitor/utils"
+)
+
+const (
+	tempPath = "/tmp/monitor"
 )
 
 func install() {
 	if os.Getegid() != 0 {
-		fmt.Println("Please run the program with sudo privileges")
+		utils.Perror("Please run the program with sudo privileges")
 		os.Exit(1)
 	}
-	fmt.Println("Installing ssdeep package...")
-	command := exec.Command("/bin/sh", "-c", "apt-get install ssdeep")
+
+	complete := true
+	fmt.Println("[-]Installing ssdeep package...")
+	command := exec.Command("/bin/sh", "-c", `wget http://downloads.sourceforge.net/project/ssdeep/ssdeep-2.13/ssdeep-2.13.tar.gz; \
+		tar zxvf ssdeep-2.13.tar.gz; cd ssdeep-2.13; ./configure; make; make install `)
+	command.Stdout = os.Stdout
+	command.Dir = tempPath
 	if err := command.Run(); err != nil {
-		fmt.Println(err)
+		utils.Perror("Unable to install ssdeep")
+		complete = false
 	}
-	fmt.Println("Installed successfully.")
-	fmt.Println("All required packages have been installed.")
-	fmt.Println("Please rerun the executable without intall option")
+	printFinalMsg(complete)
 	os.Exit(0)
+}
+
+func printFinalMsg(complete bool) {
+	if complete {
+		fmt.Println("Installed successfully.")
+		fmt.Println("All required packages have been installed.")
+		fmt.Println("Please rerun the executable without install option")
+	} else {
+		utils.Perror("Some packages were not installed")
+	}
 }
