19a052915adf1624d8bd2e77cd08801c8c6ac9ae
DarKnight <05.DarKnight.11@gmail.com>
2016-08-05 17:41:42 +0530

    [disk] Refactored disk actions

---
+++ disk/diskAction.go
@@ -0,0 +1,75 @@
+package disk
+
+import (
+	"os"
+
+	"health_monitor/utils"
+)
+
+const (
+	//DebianAPTPath is the path where downloaded apt packages are found
+	DebianAPTPath = "/var/cache/apt/archives"
+	// KALI is a constant with value "Kali"
+	KALI = "Kali"
+	// UBUNTU is a constant with value "Ubuntu"
+	UBUNTU = "Ubuntu"
+)
+
+type (
+	cleaner interface {
+		RemovePackageManagerCache() error
+		EmptyTrash() error
+	}
+	// BasicCleaner is a struct containing cleaner interface
+	BasicCleaner struct {
+		os      string
+		cleaner cleaner
+	}
+	kali struct {
+	}
+	ubuntu struct {
+	}
+)
+
+func (k kali) RemovePackageManagerCache() error {
+	return removeDebianPackageCache()
+}
+
+func (k kali) EmptyTrash() error {
+	return emptyDebianTrash()
+}
+
+func (u ubuntu) RemovePackageManagerCache() error {
+	return removeDebianPackageCache()
+}
+
+func (u ubuntu) EmptyTrash() error {
+	return emptyDebianTrash()
+}
+
+// CleanerBuilder return the struct according to the OS to clean disk space
+func CleanerBuilder(OS string) *BasicCleaner {
+	var basicCleaner cleaner
+
+	switch OS {
+	case KALI:
+		basicCleaner = kali{}
+	case UBUNTU:
+		basicCleaner = ubuntu{}
+	default:
+		basicCleaner = nil
+	}
+	return &BasicCleaner{cleaner: basicCleaner, os: OS}
+}
+
+func removeDebianPackageCache() error {
+	err := os.RemoveAll(DebianAPTPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func emptyDebianTrash() error {
+	return os.RemoveAll(utils.GetPath(".local/share/Trash/"))
+}
