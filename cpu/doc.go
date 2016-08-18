4a2f44110f9794fdde2b720e82c14bebde33e1e2
Amit Gupta <05.DarKnight.11@gmail.com>
2016-08-18 16:02:16 +0530

    Update doc.go

--- cpu/doc.go
+++ cpu/doc.go
@@ -1,6 +1,6 @@
 /*
 
-Package cpu scans the current RAM usage of the running system.
+Package cpu scans the current CPU usage of the running system.
 
 It includes function for extracting, running and exposing related data. All the parameters
 controlling the modules are in Config struct. It includes the driver for the module
